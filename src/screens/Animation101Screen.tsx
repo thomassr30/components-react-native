53bc97da22cc1e7bbb0c1dd07cc3d42a00fe817d
Thomas Soto <thomassoto94@gmail.com>
2023-03-15 20:12:24 -0300

    add: easing - bounce

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,13 +1,62 @@
 import React, { useRef } from 'react'
-import { View, StyleSheet, Animated } from 'react-native';
+import { Easing } from 'react-native';
+import { View, StyleSheet, Animated, Button } from 'react-native';
 
 const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0.4)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+    const top = useRef(new Animated.Value(-100)).current
+
+    const fadeIn = () => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 1,
+                duration: 700,
+                useNativeDriver: true
+            }
+        ).start()
+
+        Animated.timing(
+            top,
+            {
+                toValue: 0,
+                duration:700,
+                useNativeDriver: true,
+                easing: Easing.bounce
+            }
+        ).start()
+    }
+
+    const fadeOut = () => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true
+            }
+        ).start()
+    }
 
   return (
     <View style={styles.container}>
-        <Animated.View style={{...styles.purpleBox, opacity: opacity}} />
+        <Animated.View style={{
+            ...styles.purpleBox, 
+            opacity: opacity, 
+            marginBottom: 20, 
+            transform: [{
+                translateY: top
+            }]
+            }} />
+        <Button 
+            title='FadeIn'
+            onPress={fadeIn}
+        />
+        <Button 
+            title='FadeOut'
+            onPress={fadeOut}
+        />
     </View>
   )
 }
